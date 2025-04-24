e824fc3cc23a74ab11c2788550d8637796e4879c
ranjanhivoco <ranjan@hivoco.com>
2025-04-24 11:44:18 +0530

    accidental zoom remove

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,22 @@
 import "@/styles/globals.css";
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({
   subsets: ["latin"],
-  weight: ["400","600","700"],
+  weight: ["400", "600", "700"],
 });
-
-
 
 export default function App({ Component, pageProps }) {
   return (
     <main className={inter.className}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
+
       <Component {...pageProps} />
     </main>
   );
